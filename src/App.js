be4856fa4f337e262454088ce811bc3f0a50f667
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-12-18 15:01:33 -0500

    Updates the textt for the <a> tag at the bottom of the page

--- src/App.js
+++ src/App.js
@@ -9,17 +9,19 @@ function App() {
 			<header className='App-header'>
 				<h1>Hello, World!</h1>
 				<h2>This is my first serverless AWS app!</h2>
-				<img src={family} className='App-logo' alt='wife, daughter, and husband in fancy wedding attire' />
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
+				<img
+					src={family}
+					className='App-logo'
+					alt='wife, daughter, and husband in fancy wedding attire'
+				/>
+				<p>Check out my portfolio at to see more cool projects!</p>
 				<a
 					className='App-link'
-					href='https://reactjs.org'
+					href='https://hellotico.com'
 					target='_blank'
 					rel='noopener noreferrer'
 				>
-					Learn React
+					Tico's Portfolio
 				</a>
 			</header>
 		</div>
